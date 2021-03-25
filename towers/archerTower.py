9cbd50a830a78ed5eb9c490e13b583c36e133fd6
sebak <seba.k434@gmail.com>
2021-03-25 19:19:20 +0100

    Praca na wieżami łucnzików

--- towers/archerTower.py
+++ towers/archerTower.py
@@ -1,4 +1,31 @@
 import pygame
-from .tower
+from .tower import Tower
+import os
 
-class ArcherTower
+class ArcherTowerLong(Tower)
+    def __init__(self, x ,y):
+        super(). __init__(self, x, y)
+        self.tower_imgs = []
+        self.archer_imgs = []
+        self.archer_count = 0
+
+        #Wczytuje zdjęcie wiezy łuczniczej
+        for x in range(7,10):
+                self.tower_imgs.append(pygame.transform.scale(
+                    pygame.image.load(os.path.join("game_assets/archer_towers/archer_1", str(x) + ".png")),
+                    (64, 64)))
+
+        for x in range(7,10):
+                self.tower_imgs.append(pygame.transform.scale(
+                    pygame.image.load(os.path.join("game_assets/archer_towers/archer_1", str(x) + ".png")),
+                    (64, 64)))
+
+    def draw(self, win):
+        super().draw(win)
+
+    def attack(self, enemies):
+        """
+        atakuje wroga z listy wrogów, modyfikuje listę
+        :param enemies: lista wrogów
+        :return: None
+        """
--- towers/tower.py
+++ towers/tower.py
@@ -12,19 +12,50 @@ class Tower:
         self.level = 1
         self. selected = False
         self.menu = None
-        self.imgs = []
+        self.tower_imgs = []
 
     def draw(self, win):
-        pass
+        """
+        Rysowanie wieży
+        :param win: surface
+        :return: None
+        """
+        img = self.tower_imgs[self.level - 1]
+        win.blit (img, (self.x-img.get_width()//2, self.y-img.get_height()//2))
 
-    def click(self):
-        pass
+    def click(self, X, Y):
+        """
+        Zwraca gdy kliknięto na wieżę
+        i zaznacza wieżę gdy jest kliknięta
+        :param x: int
+        :param y: int
+        :return: bool
+        """
+        if X <= self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
 
     def sell(self):
-        pass
+        """
+        wezwanie do sprzedazy wiezy, zwraca koszt sprzedaży
+        :return:int
+        """
+        return self.sell_price[self.level-1]
 
     def upgrade(self):
-        pass
+        """
+        ulepsza wieżę za określony koszt
+        :return: None
+        """
+        self.level += 1
+    def get_upgrade_cost(self):
+        """
+        zwraca koszt ulepszenia, jeśli 0, to nie może  ulepszać
+        :return: int
+        """
+        return self.price(self.level-1)
 
-    def move(self):
-        pass
+    def move(self, x, y):
+        self.x = x
+        self.y = y
