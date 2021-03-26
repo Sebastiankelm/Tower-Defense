7cdf86c33edbe1b58b4f41ad5f5e90e1960f1ebc
sebak <seba.k434@gmail.com>
2021-03-26 11:36:40 +0100

    Prace nad menu

--- menu/menu.py
+++ menu/menu.py
@@ -1,19 +1,78 @@
 import pygame
+import os
+
+
+class Button:
+    def __init__(self, x, y, img, name):
+        self.name = name
+        self.img = img
+        self.x =x
+        self.y = y
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+
+    def click(self,X ,Y):
+        """
+        Zwraca gdy pozycja ma kolizje z menu
+        :param x: int
+        :param y: int
+        :return: bool
+        """
+        if X <= self.x + self.width and X >- self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
 
 class Menu:
+
     """
     menu dla przechowywanych przedmiotow
     """
-    def __init__(self, x, y):
+    def __init__(self, x, y, img):
 
         self.x = x
         self.y = y
-        self.width = 0
-        self.height = 0
+        self.width = img.get_width()
+        self.height = img.get_height()
         self.item_names = []
-        self.imgs =[]
-        self.items
+        self.buttons = []
+        self.items = 0
+        self.bg = img
 
-    def click(self,x ,y):
-        pass
+    def add_btn(self, img, name):
+        """
+        dodawanie przyciskó do menu
+        :param img: surface
+        :param name: str
+        :return: none
+        """
+        self.items += 1
+        inc_x = self.width/self.items/2
+        btn_x = self.x - self.bg.get_width()/2 + 10
+        btn_y = self.y - 120 + 10
+        self.buttons.append(Button(btn_x, btn_y, img, name))
 
+    def draw(self, win):
+        """
+        rysowanie przycisków i tła menu
+        :param win: surface
+        :return: None
+        """
+        win.blit(self.bg, (self.x - self.bg.get_width()/2, self.y-120))
+        for item in self.buttons:
+            item.draw(win)
+
+    def get_clicked(self, X, Y):
+        """
+        zwraca klikniety item z menu
+        :param X: int
+        :param Y: int
+        :return: str
+        """
+        for btn in self.buttons:
+            if btn.click(X, Y):
+                return btn.name
--- towers/tower.py
+++ towers/tower.py
@@ -1,4 +1,10 @@
 import pygame
+from menu.menu import Menu
+import os
+
+menu_bg = pygame.transform.scale(pygame.image.load(os.path.join("game_assets", "menu.png")), (120,70))
+umpgrade_btn = pygame.transform.scale(pygame.image.load(os.path.join("game_assets", "upgrade.png")), (50,50))
+
 
 class Tower:
     """
@@ -13,7 +19,11 @@ class Tower:
         self.price = [0,0,0]
         self.level = 1
         self. selected = False
-        self.menu = None
+        #definiuje menu i przyciski
+        self.menu = Menu(self.x, self.y, menu_bg)
+        self.menu.add_btn(umpgrade_btn, "Upgrade")
+
+
         self.tower_imgs = []
         self.damage = 1
 
@@ -25,6 +35,10 @@ class Tower:
         """
         img = self.tower_imgs[self.level - 1]
         win.blit (img, (self.x-img.get_width()//2, self.y-img.get_height()//2))
+
+        #rysowanie menu
+        if self.selected:
+            self.menu.draw(win)
 
     def draw_radius(self, win):
         if self.selected:
